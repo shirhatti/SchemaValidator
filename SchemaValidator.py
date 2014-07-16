4c6905597617bdfc874e753322e94dd8011bb3c4
Sourabh Shirhatti <shirhatti@outlook.com>
2014-07-16 13:04:20 -0700

    Removed test json file

--- SchemaValidator.py
+++ SchemaValidator.py
@@ -28,12 +28,9 @@ class Loading():
                 (" " * before, " " * after))
         sublime.set_timeout(lambda: self.callback(), 100)
     def clear(self):
-        # self.view.erase_status(self.status_message)
+        self.view.erase_status(self.status_message)
         pass
 
-class ExampleCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        self.view.insert(edit, 0, "Hello, World!")
 class ValidateSchemaCommand(sublime_plugin.WindowCommand):
     def run(self):
         self.thread = ValidateSchema(self.window.active_view())
@@ -48,7 +45,7 @@ class ValidateSchemaCommand(sublime_plugin.WindowCommand):
         if self.thread.result == False:
             sublime.status_message(self.thread.message)
         elif self.thread.result == True:
-            return
+            sublime.status_message(self.thread.message)
         else:
             sublime.status_message("Something went wrong. Please try again")
         return
@@ -73,7 +70,7 @@ class ValidateSchema(threading.Thread):
             self.message = schema_url
             self.result = True
         # If no schema attribute was found, let's try a file match
-        except KeyError as e:
+        except (KeyError, TypeError) as e:
             try:
                 request = urllib.request.Request("http://schemastore.org/api/json/catalog.json", headers={"User-Agent": "Sublime"})
                 http_file = urllib.request.urlopen(request, timeout=5)
@@ -95,19 +92,28 @@ class ValidateSchema(threading.Thread):
                 self.message = "%s: URL error %s contacting API" % (__name__, str(e.reason))
                 self.result = False
                 return
-            file_name = self.view.file_name()[self.view.window().folders()[0].__len__()+1:]
+            try:
+                file_name = self.view.file_name()[self.view.window().folders()[0].__len__()+1:]
+            except IndexError as e:
+                file_name = self.view.file_name()
+                if file_name == "":
+                    self.message = "Try adding a $schema attribute to your file or try saving your file"
+            schema_matched = False
             for schema_type in catalog:
                 try:
                     for file_match in schema_type["fileMatch"]:
                         # Escape the fileMatch and perform a regex search
                          if(re.compile(file_match.replace("/","\/").replace(".","\.").replace("*",".*")).match(file_name)):
                             schema_url = schema_type['url']
+                            schema_matched = True
                             break
                 # Some schemas don't have a fileMatch attribute
                 except LookupError as e:
                     pass
-            self.message = schema_url
-            self.result = True
+            if schema_matched == False:
+                self.message = "No schema could be matched based on the file name. Try adding a $schema attribute to your file"
+                self.result = False
+                return
         # Use schema_url to retrieve schema
         try:
             request = urllib.request.Request(schema_url, headers={"User-Agent": "Sublime"})
@@ -128,12 +134,15 @@ class ValidateSchema(threading.Thread):
             return
             self.result = False
         try:
-            print(jsonschema.validate(json_data, schema))
+            jsonschema.validate(json_data, schema)
         except jsonschema.exceptions.ValidationError as e:
-            dest_view = self.view.window().new_file()
-            dest_view.set_name("Schema Validation Results")
-            dest_view.run_command('content_prepend', {'text': "sadfasdfsfafs"})
-            print(e.message)
-            self.message = "JSON schema validation failed"
+            self.message = "JSON schema validation failed | %s" % e.message
+            self.result = False
             return
+        except jsonschema.exceptions.SchemaError as e:
+            self.message = "JSON schema validation failed | %s" % e.message
+            self.result = False
+            return
+        self.message = "JSON Schema successfully validated against %s" % schema_url
+        self.result = True
         return
